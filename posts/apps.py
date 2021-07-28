5026e33f2bd3f829aa3eb49a40ee0aa98f15ee61
JuanVicenteVazquezG <juanvicentevazquezg@gmail.com>
2021-07-28 14:47:42 +0200

    Minimum requirements for its operation

--- posts/apps.py
+++ posts/apps.py
@@ -2,5 +2,7 @@ from django.apps import AppConfig
 
 
 class PostsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
+    """Posts application settings."""
+    
     name = 'posts'
+    verbose_name = 'Posts'
