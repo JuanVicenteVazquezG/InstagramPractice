7f35fa4512dfe64b7ce4fce5a811cad0b47ab649
JuanVicenteVazquezG <juanvicentevazquezg@gmail.com>
2021-08-05 20:27:02 +0200

    middleware picture and biografy controller

--- d_insta/settings.py
+++ d_insta/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'd_insta.middleware.ProfileCompletionMiddleware',
 ]
 
 ROOT_URLCONF = 'd_insta.urls'
